e45d68d4537ee354683c8e3c370599111d99fdc1
Vindhya Kankurthi <vindyaj07@gmail.com>
2023-09-30 12:18:40 -0700

    weatherapp

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,6 @@
+
+
+
 console.log('javascript connected!');
 
 const carousel = new bootstrap.Carousel('#homeCarousel', {
@@ -35,3 +38,45 @@ carouselButton.addEventListener('click', function() {
         carousel.cycle();
     }
 })
+
+async function fetchWeather() {
+    console.log("hello");
+    const apiKey = process.env.OPEN_WEATHER_API_KEY
+    let city = "Seattle";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    try{
+        const response = await fetch(url);
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        displayWeather(data);
+    }
+    catch(error){
+        console.log("There was an error!", error);
+    }
+}
+fetchWeather();
+
+function displayWeather(data) {
+     const weatherIconDiv = document.querySelector('#weather-icon');
+     const description = data["weather"][0].description;
+     const icon = data["weather"][0].icon;
+     const temperature = data.main.temp;
+
+     const weatherImage = document.createElement('img');
+     weatherImage.src = `https://openweathermap.org/img/w/${icon}.png`;
+     weatherImage.alt = "weather image";
+     weatherIconDiv.appendChild(weatherImage);
+
+     const weatherTempDiv = document.querySelector('#weather-temp');
+     const temp = document.createElement('p');
+     temp.textContent = temperature + '\u00B0';
+     weatherTempDiv.appendChild(temp);
+
+     const weatherDescriptionDiv = document.querySelector('#weather-description');
+     const descriptionDiv = document.createElement('p');
+     descriptionDiv.textContent = description;
+     weatherDescriptionDiv.appendChild(descriptionDiv);
+ }
